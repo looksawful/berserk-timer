d8fc020107092e9cdfcab12bae4f42f3c37e276b
looksawful <i@lookawful.ru>
2025-02-07 23:36:32 +0300

    Adding brsrk-gui.bat to quickly run with gui

---
+++ brsrk-gui.bat
@@ -0,0 +1,8 @@
+@echo off
+REM brsk.bat: Launch Berserk Timer with default settings:
+REM  - Duration: 10 minutes
+REM  - Witness mode enabled (-w)
+REM  - Run in CLI (without -g flag)
+
+py -m src.main 10 -g -w %*
+pause
