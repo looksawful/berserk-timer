ac53963287cea73e281f8ddbcda1ecca2a2d18d9
looksawful <i@lookawful.ru>
2025-02-17 00:03:04 +0300

    hide gui runner

--- brsrk.bat
+++ brsrk.bat
@@ -1,8 +1,3 @@
 @echo off
-REM brsk.bat: Launch Berserk Timer with default settings:
-REM  - Duration: 10 minutes
-REM  - Witness mode enabled (-w)
-REM  - Run in CLI (without -g flag)
-
 py -m src.main 10 -w %*
 pause
